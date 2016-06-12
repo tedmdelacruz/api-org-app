0b4848fdaf4b778ff33f483f793e4d065e9efebb
Ted Mathew dela Cruz <tedmdelacruz@gmail.com>
2016-06-12 20:57:32 +0800

    Setup initial UI for note options for edit and delete

--- static/js/src/components/Note.js
+++ static/js/src/components/Note.js
@@ -1,21 +1,58 @@
 import React, { Component } from 'react'
 
-export class Note extends Component {
+class NoteOptions extends Component {
     render() {
-        const { id, title, text, isActive,
-            actions } = this.props
+        const { actions } = this.props
 
-        const handleClick = event => {
-            actions.selectNote(id)
+        const handleEdit = (event) => {
+            event.preventDefault()
+            actions.editNote(note)
+        }
+
+        const handleDelete = (event) => {
+            event.preventDefault()
+            actions.deleteNote(note)
         }
 
         return (
-            <div className="notes-list-item col-md-4" onClick={ handleClick }>
+            <div className="btn-group pull-right">
+                <a href="#" onClick={ handleEdit } className="btn btn-default btn-sm">
+                    <i className="fa fa-pencil"></i>
+                </a> 
+                <a href="#" onClick={ handleDelete } className="btn btn-danger btn-sm">
+                    <i className="fa fa-times"></i>
+                </a>
+            </div>
+        )
+    }
+}
+
+export class Note extends Component {
+    componentWillMount() {
+        this.setState({
+            isOptionsShown: false
+        })
+    }
+
+    render() {
+        const { id, title, text, isActive,
+            actions } = this.props
+        const note = { id, title, text }
+
+        return (
+            <div className="notes-list-item col-md-4"
+                onMouseOver={ () => { this.setState({ isOptionsShown: true }) } }
+                onMouseLeave={ () => { this.setState({ isOptionsShown: false }) } }>
+
                 <div className="panel panel-default">
                     <div className="panel-body">
                         <h4>{ title }</h4>
                         <hr/>
                         <p>{ text }</p>
+
+                        { this.state.isOptionsShown
+                            ? <NoteOptions actions={ actions }/>
+                            : null }
                     </div>
                 </div>
             </div>
