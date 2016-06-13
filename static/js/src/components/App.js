428004079cdaf27a3cbbcca8e8538d0a6c51204e
Ted Mathew dela Cruz <tedmdelacruz@gmail.com>
2016-06-13 19:10:10 +0800

    Refactor App component

--- static/js/src/components/App.js
+++ static/js/src/components/App.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react'
 import { Notes, Todo } from './index'
 
 export class App extends Component {
-    render() {
-        const { app, notes, todos, selectTab, notesActions } = this.props
-        const { activeTab } = app
+    handleTabClick(event) {
+        event.preventDefault()
+        this.props.selectTab(event.target.dataset.value)
+    }
 
-        const handleTabClick = event => {
-            event.preventDefault()
-            selectTab(event.target.dataset.value)
-        }
+    render() {
+        const { app, notes, todos, notesActions } = this.props
+        const { activeTab } = app
 
         const active = tab => {
             return (activeTab === tab) ? 'active' : null
@@ -19,14 +19,14 @@ export class App extends Component {
             <div className="container">
                 <div className="row">
                     <div className="col-md-6 col-md-offset-3">
-                        <ul className="nav nav-tabs">
+                        <ul className="nav nav-tabs nav-justified">
                             <li className={ active('notes') }>
-                                <a href="#" onClick={ handleTabClick } data-value="notes">
+                                <a href="#" onClick={ this.handleTabClick.bind(this) } data-value="notes">
                                     <i className="fa fa-sticky-note" aria-hidden="true"></i> Notes
                                 </a>
                             </li>
                             <li className={ active('todo') }>
-                                <a href="#" onClick={ handleTabClick } data-value="todo">
+                                <a href="#" onClick={ this.handleTabClick.bind(this) } data-value="todo">
                                     <i className="fa fa-check-square-o" aria-hidden="true"></i> Todo
                                 </a>
                             </li>
