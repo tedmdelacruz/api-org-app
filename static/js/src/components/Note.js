3d6ec77f3dfeab016ae7838b3aafc90cdddec4a2
Ted Mathew dela Cruz <tedmdelacruz@gmail.com>
2016-06-13 22:04:05 +0800

    Refactored Note component

--- static/js/src/components/Note.js
+++ static/js/src/components/Note.js
@@ -33,75 +33,74 @@ export class Note extends Component {
         this.props.actions.updateNote(note)
     }
 
+    handleInputChange(event) {
+        let { field } = event.target.dataset
+
+        this.setState({
+            [field]: event.target.value
+        })
+    }
+
+    handleEdit(event) {
+        if (event.target.dataset.type == 'delete') {
+            return
+        }
+
+        this.setState({ isEditMode: true })
+    }
+
+    handleDelete(event) {
+        this.setState({
+            showDelete: false,
+            isEditMode: false
+        })
+        this.props.actions.deleteNote(note)
+    }
+
+    handleInputKeydown(event) {
+        let { field } = event.target.dataset
+
+        // In title field, update note on ENTER
+        // In text field, update note on CTRL/CMD + ENTER
+        if ((field == 'title' && event.keyCode == 13) ||
+            (field == 'text' && event.keyCode == 13 && (event.metaKey || event.ctrlKey))) {
+
+            const { id, title, text } = this.state
+            const note = { id, title, text }
+            actions.updateNote(note)
+        }
+    }
+
     render() {
         const { id, title, text } = this.props
         const { actions } = this.props
-        let note = { id, title, text }
-
-        const handleInputChange = event => {
-            let { field } = event.target.dataset
-
-            this.setState({
-                [field]: event.target.value
-            })
-        }
-
-        const handleInputKeydown = event => {
-            let { field } = event.target.dataset
-
-            // In title field, update note on ENTER
-            // In text field, update note on CTRL/CMD + ENTER
-            if ((field == 'title' && event.keyCode == 13) ||
-                (field == 'text' && event.keyCode == 13 && (event.metaKey || event.ctrlKey))) {
-
-                const { id, title, text } = this.state
-                const note = { id, title, text }
-                actions.updateNote(note)
-            }
-        }
-
-        const handleEdit = event => {
-            if (event.target.dataset.type == 'delete') {
-                return
-            }
-
-            this.setState({ isEditMode: true })
-        }
-
-        const handleDelete = event => {
-            this.setState({
-                showDelete: false,
-                isEditMode: false
-            })
-            actions.deleteNote(note)
-        }
 
         const DeleteBtn = (
-            <div className="delete-note" onClick={ handleDelete } data-type="delete">
+            <div className="delete-note" onClick={ this.handleDelete.bind(this) } data-type="delete">
                 <button className="close" type="close" data-type="delete">&times;</button>
             </div>
         )
 
         const Display = (
             <div>
-                <h3>{ note.title }</h3>
-                <p>{ note.text }</p>
+                <h3>{ title }</h3>
+                <p>{ text }</p>
             </div>
         )
 
         const Form = (
             <div>
                 <input type="text" value={ this.state.title } data-field="title"
-                    onChange={ handleInputChange } onKeyDown={ handleInputKeydown }/>
+                    onChange={ this.handleInputChange.bind(this) } onKeyDown={ this.handleInputKeydown.bind(this) }/>
 
                 <textarea cols="30" rows="2" value={ this.state.text } data-field="text"
-                    onChange={ handleInputChange } onKeyDown={ handleInputKeydown }></textarea>
+                    onChange={ this.handleInputChange.bind(this) } onKeyDown={ this.handleInputKeydown.bind(this) }></textarea>
             </div>
         )
 
         return (
             <div className="notes-list-item col-md-12"
-                onClick={ handleEdit }
+                onClick={ this.handleEdit.bind(this) }
                 onMouseEnter={ () => { this.setState({ showDelete: true }) } }
                 onMouseLeave={ this.handleMouseLeave.bind(this) }>
 
