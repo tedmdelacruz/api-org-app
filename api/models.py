271c03366379a2eece08220ba49eea70a2d248e1
Ted Mathew dela Cruz <tedmdelacruz@gmail.com>
2016-06-15 15:19:58 +0800

    Add default value for Todo.is_done field

--- api/models.py
+++ api/models.py
@@ -11,7 +11,7 @@ class Note(models.Model):
 
 class Todo(models.Model):
     entry = models.CharField(max_length=100)
-    is_done = models.BooleanField()
+    is_done = models.BooleanField(default=False)
 
     def __str__(self):
         return self.entry
